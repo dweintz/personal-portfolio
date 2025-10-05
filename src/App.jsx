9b342b4b28d83d2ed3501bba1ee976c0b0bc3471
dweintz <dweintz@purdue.edu>
2025-10-05 17:33:05 -0400

    Fixed background and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
-import Hero from './Components/Hero/Hero'
 import AboutBox from './Components/AboutBox/AboutBox'
 import Footer from './Components/Footer/Footer'
 
+
 const App = () => {
   return (
-    <div className="bg-gray-100 min-h-screen">
+    <div className="app">
       <Navbar/>
-      <Hero/>
+      <main className="main-content">
+        <section className="about-section">
+          <AboutBox />
+        </section>
+      </main>
       <Footer/>
     </div>
   )
 }
 
 export default App
-
