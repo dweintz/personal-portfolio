076aa3b4ac566030e2af58ba08a1e6d2b888619d
dweintz <dweintz@purdue.edu>
2025-10-19 19:37:28 -0400

    Changed formatting

--- src/Pages/WorkExperience/Experiences/Chipotle.jsx
+++ src/Pages/WorkExperience/Experiences/Chipotle.jsx
@@ -7,7 +7,7 @@ const Chipotle = () => {
   return (
     <div className="project-box slide-box slide-right">
       <div className="project-header">
-    
+        <h2 className="project-title">Chipotle Mexican Grill</h2>
         <img src={chipotleLogo} alt="GE" className="project-logo" />
       </div>
       <h3 className="project-role">Crew Member</h3>
