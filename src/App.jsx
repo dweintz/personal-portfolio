f19b8ef831eb5aad99244c5d020b129031b7d1bd
dweintz <dweintz@purdue.edu>
2025-10-19 18:58:23 -0400

    Cleaned up code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import AboutBox from "./AboutBox/AboutBox";
 
 const Home = () => {
